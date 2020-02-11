73f5cb35984fd2fa25259f3d6d69ad7167561c6f
Leonid <leonidbarsucovschi0503@gmail.com>
2020-02-11 11:58:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+/*let person = {
+    name: "John",
+    age: 24,
+    isMarried: false
+
+};
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[1]); */
+
+
+/*let person = {
+    name: "Leonid",
+    age: 26,
+    noMarried: false
+};
+console.log(person.name);
+alert(person.name);*/
+
+// let array = ['plumb.png','orange.jpg','apple.bmp'];
+// console.log(array[2]);
+// alert(array[2]);
+
+//alert ("Hello World");
+//confirm("Are you here?");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+//  let answer = prompt("Do you have 19?","YES");
+//  console.log(typeof(array));
+
+// let cotik = function(howManyTimes)
+// {
+//     for (var i=0;i<howManyTimes;i++){
+//         console.log(i+"=^.^=");
+//     }
+    
+// };
+// cotik(13);
+
+// function pow(x, n) {
+//     var result = 1;
+
+//     for (var i=0; i<n; i++){
+//         result *= x;
+//     }
+//     return result;
+// }
+
+// var x = prompt("x?", "");
+// var n = prompt("n?", "");
+
+// if (n < 0 ) {
+//     alert('Степень' + n +
+//     'не поддерживается, введите целую степень, большую 0');
+// } else {
+//     alert( pow(x, n) );
+// }
+
+//  let fact = 1;
+//  for ( var i=1; i<10; i++){
+//      fact = fact*i;
+//      document.write(i + "! = " + fact + "<br>");
+//  }
+
+// let answer = +prompt("Do you have 19?","YES");
+// console.log(typeof(answer));
+
+//  let array = ["plumb.png", "orange.jpg","apple.bmp"];
+//  console.log(array[2]);
+
+
+// console.log("arr" + " - object");
+// console.log(5 +  + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2"=== 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
